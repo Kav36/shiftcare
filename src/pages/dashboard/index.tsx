689a47dffd1c2faf3be006181e08556dca42a18d
Kav36 <kavindu.h.g.g@gmail.com>
2025-03-21 13:58:11 +0530

    first commit

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import Stack from '@mui/material/Stack';
+
+const Dashboard = () => {
+  return (
+    <Stack spacing={3}>
+      
+      
+      
+    </Stack>
+  );
+};
+
+export default Dashboard;
